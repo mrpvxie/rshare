b03320de6276f88e92c4c8674054f32a86682ed8
rahul271203 <killkamam3@gmail.com>
2024-09-10 02:05:00 +0530

    search filter backend

--- static/js/table_data.js
+++ static/js/table_data.js
@@ -21,32 +21,32 @@ function insert_data(data_array) {
   const row_data = data_array
     .map((currentItem) => {
       return `<tr>
-                    <div id="modal_no_{{ ${currentItem.id} }}" class="modal" style="display:none">
+                    <div id="modal_no_${currentItem.id}" class="modal" style="display:none">
                         <div class="modal-content"> 
                             <span class="close">&times;</span>
                             <p>Are you sure you want to delete this item?</p>
                             <div class="modal-buttons">
-                                <a href="{{url_for('delete_content', content_id= ${currentItem.id})}}" class ="delete_yes_button">
+                                <a href="{url_for("delete_content",(content_id = ${currentItem.id}))}" class ="delete_yes_button">
                                     <button class="btn-yes">Yes</button>
                                 </a>
                                 <button class="btn-no">No</button>
                             </div>
                         </div>
                     </div>
-                <td data-label="id" class = "table_id">{{ ${currentItem.id} }}</td>  
+                <td data-label="id" class="table_id">${currentItem.id}</td>  
 
-                <td data-label="content" class="content_column table_content"><a href = "{{ url_for('full_content',content_id = ${currentItem.id}) }}">
-                    
-                        <a href = "{{ url_for('full_content',content_id = cont.id) }}">{{ ${currentItem.content}[:80]|safe }}</a>  
-                    
-                </a></td>
-                <td data-label="time" class="table_time">{{ ${currentItem.time} }}</td>   
+                <td data-label="content" class="content_column table_content">
+                    <a href="{url_for(
+                      "full_content",
+                      (content_id = ${currentItem.id})
+                    )}">${currentItem.content}</a>  
+                </td>
+                <td data-label="time" class="table_time">${currentItem.time}</td>   
 
-                <td id="deleteBtn{{ ${currentItem.id} }}" class="table_delete">  
-
-                        <button class="btn_trash delete_button" data-label="modal_no_{{ ${currentItem.id} }}">
-                            <i class="fa fa-trash"></i>
-                        </button>
+                <td id="deleteBtn${currentItem.id}" class="table_delete">  
+                    <button class="btn_trash delete_button" data-label="modal_no_${currentItem.id}">
+                        <i class="fa fa-trash"></i>
+                    </button>
                 </td>
             </tr>`;
     })
@@ -55,24 +55,23 @@ function insert_data(data_array) {
 }
 
 const search_input = document.querySelector(".search_input");
-console.log(search_input.value);
-search_input.addEventListener("keydown", function () {
-  if (search_input.value == "") {
+
+search_input.addEventListener("input", function () {
+  const searchValue = search_input.value.toLowerCase().trim();
+  if (searchValue === "") {
     insert_data(content_data);
-    console.log("this is empty");
-  }
-  const filtered_products = content_data.filter(function (currentItem) {
-    if (currentItem.content.includes(search_input.value.toLowerCase())) {
-      return currentItem;
+    console.log("Showing all data");
+  } else {
+    const filtered_products = content_data.filter(function (currentItem) {
+      return currentItem.content.toLowerCase().includes(searchValue);
+    });
+    if (filtered_products.length === 0) {
+      insert_data([]);
+      console.log("No results found");
     } else {
       insert_data(filtered_products);
     }
-  });
-  console.log(filtered_products);
-  insert_data(filtered_products);
-  if (filtered_products.length === 0) {
-    document.querySelector(
-      ".products"
-    ).innerHTML = `<span class="no-product">No product with this name was found</span>`;
+
+    console.log(filtered_products);
   }
 });
